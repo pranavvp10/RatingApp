c67a5867f10ebe5b45dbe813e4c826ee74078fd8
pranav vp <pranavvp07@gmail.com>
2020-10-30 14:04:06 +0530

    Updates

---
+++ precache-manifest.745432f6da2ca0e59fa07c2cdf910c95.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9141495ea3f1c5b946e4",
+    "url": "/RatingApp/static/js/main.f4d541c6.chunk.js"
+  },
+  {
+    "revision": "384e115ff61b0afebd26",
+    "url": "/RatingApp/static/js/runtime~main.4441c4c5.js"
+  },
+  {
+    "revision": "4bab3774e7c922ffc0dc",
+    "url": "/RatingApp/static/js/2.8e24d8a7.chunk.js"
+  },
+  {
+    "revision": "430a10652ac55202ebc81803f8a57619",
+    "url": "/RatingApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pranavvp10/RatingApp/precache-manifest.a97b39ca947c1c17430d961dda216bc6.js"
+  "/RatingApp/precache-manifest.745432f6da2ca0e59fa07c2cdf910c95.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pranavvp10/RatingApp/index.html", {
+workbox.routing.registerNavigationRoute("/RatingApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
